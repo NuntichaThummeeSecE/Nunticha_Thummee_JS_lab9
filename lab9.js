fe6495b1df02a02bf5e609975557f9a865974f35
NuntichaThummeeSecE <n01687478@humber.ca>
2024-11-13 12:05:43 -0500

    create a class

--- lab9.js
+++ lab9.js
@@ -1,22 +1,26 @@
 //task 1 
-//create object : student
-let student = {
-    name: "Mark",
-    age: 15,
-    grade: 86,
-    school: "Riverdale",
+//create class : student
+class student {
+    constructor(name, age, grade, school){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.school = school;
+    }
 
     //create greet method
     greet(){
-        console.log("Hello " + student.name);
+        console.log("Hello " + this.name);
     }
 }
 
+let student1 = new student ("Mark", 15, 86, "Riverdale");
+
 //call method
-student.greet();
+student1.greet();
 
 //console.log each property of the student object
-console.log("Name: "+student.name);
-console.log("Age: "+student.age);
-console.log("Grade: "+student.grade);
-console.log("School: "+student.school);
+console.log("Name: "+student1.name);
+console.log("Age: "+student1.age);
+console.log("Grade: "+student1.grade);
+console.log("School: "+student1.school);
